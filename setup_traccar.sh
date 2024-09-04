ff966c51864be50672640fc4af0d70281bc02a55
gv-sh <108992402+gv-sh@users.noreply.github.com>
2024-09-04 12:11:28 +1000

    update script

--- setup_traccar.sh
+++ setup_traccar.sh
@@ -14,29 +14,29 @@ echo "Pulling latest changes from GitHub repository..."
 git pull origin main
 
 echo "Generating self-signed SSL certificates..."
-mkdir -p certs
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout certs/selfsigned.key -out certs/selfsigned.crt -subj "/CN=${EC2_PUBLIC_IP}"
+mkdir -p $CERTS_DIR
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $CERTS_DIR/selfsigned.key -out $CERTS_DIR/selfsigned.crt -subj "/CN=${EC2_PUBLIC_IP}"
 
 echo "Configuring Nginx with EC2 Public IP..."
-sed -i "s/\${EC2_PUBLIC_IP}/${EC2_PUBLIC_IP}/g" nginx/nginx.conf
+sed -i "s/\${EC2_PUBLIC_IP}/${EC2_PUBLIC_IP}/g" $NGINX_DIR/nginx.conf
 
 echo "Cloning Traccar repository..."
-git clone --recursive https://github.com/traccar/traccar.git backend/traccar
+git clone --recursive https://github.com/traccar/traccar.git $APP_DIR
 
 echo "Setting up Traccar configuration..."
-mkdir -p conf
-if [ ! -f conf/traccar.xml ]; then
+mkdir -p $CONF_DIR
+if [ ! -f $CONF_DIR/traccar.xml ]; then
     echo "Copying default traccar.xml configuration..."
-    cp backend/traccar/setup/traccar.xml conf/traccar.xml
+    cp $APP_DIR/setup/traccar.xml $CONF_DIR/traccar.xml
 fi
 
 # Update database configuration in traccar.xml
-sed -i 's|<entry key="database.url">.*</entry>|<entry key="database.url">'${DATABASE_URL}'</entry>|' conf/traccar.xml
-sed -i 's|<entry key="database.user">.*</entry>|<entry key="database.user">'${DATABASE_USER}'</entry>|' conf/traccar.xml
-sed -i 's|<entry key="database.password">.*</entry>|<entry key="database.password">'${DATABASE_PASSWORD}'</entry>|' conf/traccar.xml
+sed -i 's|<entry key="database.url">.*</entry>|<entry key="database.url">'${DATABASE_URL}'</entry>|' $CONF_DIR/traccar.xml
+sed -i 's|<entry key="database.user">.*</entry>|<entry key="database.user">'${DATABASE_USER}'</entry>|' $CONF_DIR/traccar.xml
+sed -i 's|<entry key="database.password">.*</entry>|<entry key="database.password">'${DATABASE_PASSWORD}'</entry>|' $CONF_DIR/traccar.xml
 
 echo "Setting up webapp..."
-cp -r backend/traccar/traccar-web/* webapp/
+cp -r $APP_DIR/traccar-web/* $WEBAPP_DIR/
 
 echo "Building and starting Docker containers..."
 docker-compose up -d --build
